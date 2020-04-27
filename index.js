b79f8e6fd21d862653214c723892c8ae2f45c988
sabrinalagasse <lagassesabrina@gmail.com>
2020-04-27 10:25:40 -0700

    Test joovooscropt

--- index.js
+++ index.js
@@ -1,53 +1,8 @@
-// class Calculator {
-//   constructor(prevOp, currentOp) {
-//     this.prevOp = prevOp;
-//     this.currentOp = currentOp;
-//   }
-
-//   allClear() {
-//     alert("clicked all clear button");
-//   }
-
-//   del() {
-//     alert("clicked delete button");
-//   }
-
-//   chooseOperation() {
-//     alert("clicked operation button");
-//   }
-
-//   addendNumber() {
-//     alert("clicked number button");
-//   }
-
-//   performCalc() {
-//     alert("perform calc time!");
-//   }
-
-//   updateDisplay() {
-//     alert("updating display!");
-//   }
-// }
-
-alert("test!");
+alert("this is some joovooscropt!");
 
 function allClear() {
-  alert("clicked all clear button");
+  alert("clicked the all clear button");
 }
 
-// //create calculator object
-// const myCalc = new Calculator("", "");
-
-//access buttons
-const allClearButton = document.querySelector("[data-all-clear");
-allClearButton.addEventListener("click", allClear());
-
-// const numberButtons = document.querySelectorAll("[data-number");
-// const operationButtons = document.querySelectorAll("[data-operaton]");
-
-// for (const numButton of numberButtons) {
-//   numButton.addEventListener("click", () => {
-//     myCalc.addendNumber();
-//     myCalc.updateDisplay();
-//   });
-// }
+const allClearButton = document.querySelector("[data-all-clear]");
+allClearButton.addEventListener("click", allClear);
